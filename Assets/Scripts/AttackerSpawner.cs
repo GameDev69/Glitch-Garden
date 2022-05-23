c0d4826a122a4326f7166f62e2f7f63831ec8998
Iskandarus <iskandarus337@gmail.com>
2022-05-23 12:16:09 +0500

    Coroutine for attackers
    Создан новый Coroutine метод для attackers. Построен ряд из spawn'еров с Coroutine методами

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private float minSpawnDelay;
+    [SerializeField] private float maxSpawnDelay;
+    [SerializeField] private Attacker attackerPrefab;
+    
+    private bool _spawn = true;
+    
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (_spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
