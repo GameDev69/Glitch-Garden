f8b1be5b117f3c3b68cf09bfd2b3337ea530d5ed
Iskandarus <iskandarus337@gmail.com>
2022-05-28 11:59:06 +0500

    Gun shooter
    Теперь кактус может стрелять через объект Gun

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] private GameObject projectile, gun;
+    
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
