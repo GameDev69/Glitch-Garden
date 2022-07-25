a1a547e4ea103477e9ca787f88f0ae6f11e47fce
Iskandarus <35442045+Iskandarus@users.noreply.github.com>
2022-07-25 22:35:40 +0500

    Added new Defender (Gnome) !
    Добавлен и полностью настроен новый тип защитника Гном

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -21,12 +21,12 @@ public class Shooter : MonoBehaviour
     {
         if (IsAttackerInLane())
         {
-            // Debug.Log("Он на моей линии");
+            Debug.Log("Он на моей линии");
             _animator.SetBool(_isAttacking, true);
         }
         else
         {
-            // Debug.Log("Жду...");
+            Debug.Log("Жду...");
             _animator.SetBool(_isAttacking, false);
         }
     }
@@ -39,9 +39,11 @@ public class Shooter : MonoBehaviour
             bool isCloseEnough = 
                 (Mathf.Abs(spawner.transform.position.y - transform.position.y) 
                  <= 0.2f);
-            // Debug.Log(spawner.name);
-            // Debug.Log(Mathf.Abs(spawner.transform.position.y - transform.position.y) 
-            //           <= 0.2f);
+            Debug.Log(spawner.name);
+            Debug.Log(Mathf.Abs(spawner.transform.position.y - transform.position.y) 
+                      <= 0.2f);
+            Debug.Log(spawner.transform.position.y);
+            Debug.Log(transform.position.y);
             if (isCloseEnough)
             {
                 _myLaneSpawner = spawner;
