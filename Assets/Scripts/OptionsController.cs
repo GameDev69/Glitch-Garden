b5ae1e407e36e3cad9a4dd88e3b382229462e006
Iskandarus <35442045+Iskandarus@users.noreply.github.com>
2022-07-31 22:27:17 +0500

    Added music control mechanism
    Теперь можно контролировать громкость музыки через Options Scene
    Имеются ошибки!! - Don't destroy on load

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private float defaultVolume;
+    
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    void Update()
+    {
+        MusicPlayer musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.Log("No music player found... did you start from splash screen?");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        Debug.Log(volumeSlider.value + " " + PlayerPrefsController.GetMasterVolume());
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
